18284e72f2baa505258a6a0e2118ed3d7adef88e
daiarobert <daiarobert2@gmail.com>
2021-12-08 20:44:45 +0200

    test

--- src/components/articlesPage/Articles.jsx
+++ src/components/articlesPage/Articles.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import './articles.scss';
 import Axios from 'axios';
 //import ArticleItem from './ArticleItem';
